014bf7258428a0463039e7c73f017b0b560361e3
RobTosPro <robtospro@gmail.com>
2025-04-02 17:35:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bloxlaunch Website
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="font-montserrat max-w-screen overflow-x-hidden bg-amber-300"> 
+      <h1 className="font-bold px-2 text-4xl tracking-tighter text-balance max-lg:font-medium max-sm:px-4 sm:text-5xl lg:text-6xl xl:text-8xl">Bloxlaunch</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
